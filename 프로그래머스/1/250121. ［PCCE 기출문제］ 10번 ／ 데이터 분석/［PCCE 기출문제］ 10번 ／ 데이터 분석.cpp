a2d51023512e8144f1c98edd89a4687cbe171852
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-03-25 13:56:14 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.48 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> answer;
+int ext_idx, sort_idx;
+string arr[4] = {"code", "date", "maximum", "remain"};
+
+bool comp(vector<int> v1, vector<int> v2) {
+    if(v1[sort_idx] < v2[sort_idx]) return true;
+    else return false;
+}
+vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
+    for(int i = 0; i < 4; ++i) {
+        if(arr[i] == ext) ext_idx = i;
+        if(arr[i] == sort_by) sort_idx = i;
+    }
+    for(auto d : data)
+        if(d[ext_idx] < val_ext) answer.push_back(d);
+    sort(answer.begin(), answer.end(), comp);
+    return answer;
+}
