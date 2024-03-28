ad7108e0e2da99b680de60205738707981cdc2d8
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-03-28 23:33:06 +0900

    [level 2] Title: 피보나치 수, Time: 0.51 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int arr[100001];
+int solution(int n) {
+    arr[0] = 0; arr[1] = 1; arr[2] = 1;
+    for(int i = 3; i <= n; ++i)
+        arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
+    return arr[n];
+}
