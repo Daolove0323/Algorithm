e001f4119963c7d0f3a4db91a9dcac5f19eb8847
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-04-01 14:16:00 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b) {
+    for(int i = 1;;++i) {
+        if((a % 2 == 1 && a + 1 == b) || (b % 2 == 1 && b + 1 == a)) return i;
+        a = (a - 1) / 2 + 1;
+        b = (b - 1) / 2 + 1;
+    }
+}
