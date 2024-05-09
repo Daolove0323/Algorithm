2771d3f072cddf481b703e14233783eb4fd9d90c
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-05-09 11:11:02 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,30 +1,17 @@
 #include <iostream>
-#include <cstring>>
 using namespace std;
 
-int x;
-int arr[1000001];
+int N;
+int dp[1000001];
 
-void recursive(int num){
-    if(num == 1) return;
-    if(num % 2 == 0 && arr[num / 2] > arr[num] + 1){
-        arr[num / 2] = arr[num] + 1;
-        recursive(num / 2);
+int main() {
+    cin >> N;
+    for(int i = 0; i <= 1000000; ++i) dp[i] = 987654321;
+    dp[1] = 0;
+    for (int i = 1; i <= N; ++i) {
+        if(i + 1 <= N) dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+        if(i * 2 <= N)  dp[i * 2] = min(dp[i * 2], dp[i] + 1);
+        if(i * 3 <= N)  dp[i * 3] = min(dp[i * 3], dp[i] + 1);
     }
-    if(num % 3 == 0 && arr[num / 3] > arr[num] + 1){
-        arr[num / 3] = arr[num] + 1;
-        recursive(num / 3);
-    }
-    if(arr[num - 1] > arr[num] + 1){
-    arr[num - 1] = arr[num] + 1;
-    recursive(num - 1);
-    }
-}
-
-int main(void){
-    cin >> x;
-    memset(arr, 1000000, sizeof(arr));
-    arr[x] = 0;
-    recursive(x);
-    cout << arr[1];
+    cout << dp[N];
 }
