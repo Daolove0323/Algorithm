fb24bdc32fdbc24bff751a308a909678b4332b60
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-05-20 16:15:50 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,47 +1,43 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
-#include <utility>
 using namespace std;
 
-pair<int, int> direction[4] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-int N, M;
-int maze[100][100];
-bool visited[100][100];
+struct pos {
+    int y, x, cnt;
+};
 
-void bfs(int i, int j){
-	queue<pair<int, int>> q;
-	pair<int, int> p = make_pair(i, j);
-	q.push(p);
-	maze[i][j] = 1;
-	visited[i][j] = true;
+int dx[] = {0, 0, -1, 1}, dy[] = {-1, 1, 0, 0};
+int n, m;
+string s;
+bool canMove[100][100], checked[100][100];
+queue<pos> q;
 
-	while(!q.empty()){
-		p = q.front();
-		q.pop();
-		for(int i = 0; i < 4; i++){
-			pair<int, int> tmp = make_pair(p.first + direction[i].first, p.second + direction[i].second);
-			if(tmp.first < 0 || N <= tmp.first
-			|| tmp.second < 0 || M <= tmp.second
-			|| maze[tmp.first][tmp.second] == 0 || visited[tmp.first][tmp.second] == true)
-				continue;
-			q.push(tmp);
-			visited[tmp.first][tmp.second] = true;
-			maze[tmp.first][tmp.second] = maze[p.first][p.second] + 1;
-		}
-	}
-
-}
-
-int main(void)
-{
-	cin >> N >> M;
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			int n;
-			scanf("%1d", &n);
-			maze[i][j] = n;
-		}
-	}
-	bfs(0, 0);
-	cout << maze[N-1][M-1];
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        cin >> s;
+        for (int j = 0; j < m; ++j) {
+            if (s[j] == '1') canMove[i][j] = true;
+            else canMove[i][j] = false;
+        }
+    }
+    
+    q.push({0, 0, 0});
+    checked[0][0] = true;
+    while (!q.empty()) {
+        int y = q.front().y;
+        int x = q.front().x;
+        int cnt = q.front().cnt;
+        if (y == n - 1 && x == m - 1) break;
+        q.pop();
+        for (int i = 0; i < 4; ++i) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (n <= ny || ny < 0 || m <= nx || nx < 0 || !canMove[ny][nx] || checked[ny][nx])  continue;
+            q.push({ny, nx, cnt + 1});
+            checked[ny][nx] = true;
+        }
+    }
+    cout << q.front().cnt + 1;
 }
