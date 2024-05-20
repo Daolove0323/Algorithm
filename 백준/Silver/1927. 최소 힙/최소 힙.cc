2ecb8a4750a9222cf8e1392f5dc9116436065122
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-05-20 13:54:43 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -2,26 +2,18 @@
 #include <queue>
 using namespace std;
 
-int N, input;
+int n, m;
 priority_queue<int, vector<int>, greater<int>> pq;
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
-    cin >> N;
-    while(N--) {
-        cin >> input;
-        if(input == 0) {
-            if(pq.empty())
-                cout << 0 << '\n';
-            else {
-                cout << pq.top() << '\n';
-                pq.pop();
-            }
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> n;
+    while (n--) {
+        cin >> m;
+        if (m > 0) pq.push(m);
+        else if (m == 0) {
+            if (pq.empty()) cout << 0 << '\n';
+            else {cout << pq.top() << '\n'; pq.pop();}
         }
-        else
-            pq.push(input);
     }
 }
