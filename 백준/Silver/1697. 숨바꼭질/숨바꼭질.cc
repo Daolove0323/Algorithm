9a8ae05f3fe056cd08139f7f27cc29f8d10e377d
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-05-20 13:43:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 16 ms, Memory: 5476 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,40 +1,23 @@
 #include <iostream>
 #include <queue>
+#include <utility>
 using namespace std;
 
-const int maxIdx = 200001;
+queue<pair<int, int>> q;
+bool checked[100001];
+int n, k;
 
-int N, K;
-int dp[maxIdx];
-
-void bfs(){
-	queue<int> q;
-	q.push(N);
-
-	while(true){
-		int tmp = q.front();
-		if(tmp == K) break;
-		q.pop();
-
-		if(dp[tmp + 1] == 0 && 0 <= tmp + 1 && tmp + 1 < maxIdx){
-		q.push(tmp + 1);
-		dp[tmp + 1] = dp[tmp] + 1;
-		}
-
-		if(dp[tmp - 1] == 0 && 0 <= tmp -1 && tmp -1 < maxIdx){
-		q.push(tmp - 1);
-		dp[tmp - 1] = dp[tmp] + 1;
-		}
-
-		if(dp[tmp * 2] == 0 && 0 <= tmp * 2 && tmp * 2 < maxIdx){
-		q.push(tmp * 2);
-		dp[tmp * 2] = dp[tmp] + 1;
-		}
-	}
-}
-
-int main(void){
-	cin >> N >> K;
-	bfs();
-	cout << dp[K];
+int main() {
+    cin >> n >> k;
+    q.push({n, 0});
+    checked[n] = true;
+    while(q.front().first != k) {
+        int f = q.front().first, s = q.front().second;
+        checked[f] = true;
+        if (0 <= f + 1 && f + 1 <= 100001 && !checked[f + 1]) q.push({f + 1, s + 1});
+        if (0 <= f - 1 && f - 1 <= 100001 && !checked[f - 1]) q.push({f - 1, s + 1});
+        if (0 <= f * 2 && f * 2 <= 100001 && !checked[f * 2]) q.push({f * 2, s + 1});
+        q.pop();
+    }
+    cout << q.front().second;
 }
