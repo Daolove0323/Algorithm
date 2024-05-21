47a0bd074b63aacef4cdfae8771a96796c08560c
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-05-21 10:34:44 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,20 +2,16 @@
 #include <algorithm>
 using namespace std;
 
-int n;
-int arr[302];
-long dp[2][302];
+int dp[2][302], points[302], n;
 
-int main(void){
+int main() {
     cin >> n;
-    for(int i = 1; i <= n; i++) cin >> arr[i];
-    
-    dp[0][1] = dp[1][1] = arr[1];
-    dp[0][2] = arr[2];
-    for(int i = 0; i < n; i++){        
-        dp[1][i + 1] = dp[0][i] + arr[i + 1];
-        dp[0][i + 2] = max(dp[0][i], dp[1][i]) + arr[i + 2];
+    for (int i = 1; i <= n; ++i) cin >> points[i];
+    dp[1][1] = points[1];
+    dp[0][2] = points[2], dp[1][2] = dp[1][1] + points[2];
+    for (int i = 3; i <= n; ++i) {
+        dp[0][i] = max(dp[0][i - 2], dp[1][i - 2]) + points[i];
+        dp[1][i] = dp[0][i - 1] + points[i];
     }
-    
     cout << max(dp[0][n], dp[1][n]);
 }
