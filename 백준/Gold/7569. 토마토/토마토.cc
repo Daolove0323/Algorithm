3fdde25026c84e6c239a1ef3c3b11cbf08cfb463
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-07-10 21:26:05 +0900

    [Gold V] Title: 토마토, Time: 272 ms, Memory: 8868 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,67 +1,66 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 
-int ripe[6][3] = {{-1, 0, 0}, {1, 0, 0}, {0, -1, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1}};
+struct pos {
+    int z, y, x;
+};
 
-int M, N, H;
-int day = -1;
-int box[100][100][100]; // -1 : 빈 / 0 : 안익은 / 1 : 익은
-vector<vector<int>> v;
+int dx[] = {-1, 1, 0, 0, 0, 0}, dy[] = {0, 0, -1, 1, 0, 0}, dz[] = {0, 0, 0, 0, -1, 1};
+bool completed;
+int h, m ,n, day;
+int arr[101][101][101];
+queue<pos> ripe;
 
-bool allRiped(){
-    for(int i = 0; i < H; i++)
-        for(int j = 0; j < N; j++)
-            for(int k = 0; k < M; k++)
-                if(box[i][j][k] == 0)
+bool passDay() {
+    bool changed = false;
+    queue<pos> next;
+    while (!ripe.empty()) {
+        pos p = ripe.front();
+        ripe.pop();
+        for (int i = 0; i < 6; ++i) {
+            int nx = p.x + dx[i];
+            int ny = p.y + dy[i];
+            int nz = p.z + dz[i];
+            if (!(0 <= nz && nz < h) || !(0 <= ny && ny < m) || !(0 <= nx && nx < n) || arr[nz][ny][nx] != 0) continue;
+            next.push({nz, ny, nx});
+            arr[nz][ny][nx] = 1;
+            changed = true;
+        }
+    }
+    ripe = next;
+    if (changed) ++day;
+    return changed;
+}
+
+bool isAllRipe() {
+    for (int i = 0; i < h; ++i)
+        for (int j = 0; j < m; ++j)
+            for (int k = 0; k < n; ++k)
+                if (arr[i][j][k] == 0)
                     return false;
     return true;
 }
 
-void bfs(){
-    queue<vector<int>> q;
-    for(int i = 0; i < v.size(); i++)
-        q.push({v[i][0], v[i][1], v[i][2]});
-    
-    while(!q.empty()){
-        int repeat = q.size();
-        bool changed = false;
-
-        while(repeat--){
-            vector<int> tmp = q.front();
-            for(int i = 0; i < 6; i++){
-                int nz = tmp[0] + ripe[i][0];
-                int ny = tmp[1] + ripe[i][1];
-                int nx = tmp[2] + ripe[i][2];
-                if(H <= nz || nz < 0 || N <= ny || ny < 0 || M <= nx || nx < 0 ||
-                    box[nz][ny][nx] != 0) continue;
-
-            q.push({nz, ny, nx});
-            box[nz][ny][nx] = 1;
-            changed = true;
-            } 
-            q.pop();
-        } 
-        day++;
-    }
-}
-
-int main(void){
-    ios_base::sync_with_stdio(false);
-    cout.tie(0);
-    cin.tie(0);
-
-    cin >> M >> N >> H;
-    for(int i = 0; i < H; i++)
-        for(int j = 0; j < N; j++)
-            for(int k = 0; k < M; k++){
-                cin >> box[i][j][k];
-                if(box[i][j][k] == 1)
-                    v.push_back({i, j, k});
+int main() {
+    cin >> n >> m >> h;
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < m; ++j) {
+            for (int k = 0; k < n; ++k) {
+                cin >> arr[i][j][k];
+                if (arr[i][j][k] == 1) {
+                    ripe.push({i, j ,k});
+                }
             }
-                
-    bfs();
-    if(allRiped()) cout << day;
+        }
+    }
+    
+    while(!completed) {
+        completed = true;
+        if (passDay())
+            completed = false;
+    }
+    
+    if (isAllRipe()) cout << day;
     else cout << -1;
 }
