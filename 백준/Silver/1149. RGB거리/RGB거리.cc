7827582fae455cdda23e1883cd52b45c98facabb
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-07-17 21:43:54 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,19 +1,38 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int N;
-int cost[3][1001], dp[3][1001];
+int n, cost_buf;
+int cost[1000][3];
+int dp[1000][3];
 
-void func(int i){
-    dp[0][i] = min(dp[1][i - 1], dp[2][i - 1]) + cost[0][i];
-    dp[1][i] = min(dp[0][i - 1], dp[2][i - 1]) + cost[1][i];
-    dp[2][i] = min(dp[0][i - 1], dp[1][i - 1]) + cost[2][i];
+void init() {
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < 3; ++j)
+            dp[i][j] = 987654321;
 }
 
-int main(void){
-    cin >> N;
-    for(int i = 1; i <= N; i++) cin >> cost[0][i] >> cost[1][i] >> cost[2][i];
-    dp[0][0] = cost[0][0], dp[0][1] = cost[0][1], dp[0][2] = cost[0][2];
-    for(int i = 1; i <= N; i++) func(i);
-    cout << min(min(dp[0][N], dp[1][N]), dp[2][N]);
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cin >> cost_buf;
+            cost[i][j] = cost_buf;
+        }
+    }
+    init();
+    for (int i = 0; i < 3; ++i)
+        dp[0][i] = cost[0][i];
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (j == 0) {
+                dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+            } else if (j == 1) {
+                dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+            } else if (j == 2) {
+                dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+            }
+        }
+    }
+    cout << min({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]});
 }
