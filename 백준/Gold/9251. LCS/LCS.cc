b05d82d74bfbc01a547e0b239fe6c896912ce72e
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-07-22 15:12:29 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -2,45 +2,16 @@
 #include <string>
 using namespace std;
 
-int max(int n1, int n2)
-{
-	if(n1>n2)
-		return n1;
-	else
-		return n2;
-}
+string s1, s2;
+int dp[1001][1001];
 
-int main(void)
-{
-	int dp[1001][1001];
-	string s1, s2;
-	cin>>s1>>s2;
-	
-	for(int i = 0; i <= s1.size(); i++)
-		dp[i][0] = 0;
-	for(int i = 0; i <= s2.size(); i++)
-		dp[0][i] = 0;
-
-	for(int row = 1; row <= s1.size(); row++)
-	{
-		for(int col = 1; col <= s2.size(); col++)
-		{
-			if(s1[row-1] == s2[col-1])
-				dp[row][col] = dp[row-1][col-1] + 1;
-			else
-				dp[row][col] = max(dp[row-1][col], dp[row][col-1]);
-		}
-	}
-
-	// for(int i = 0; i <= s1.size(); i++)
-	// {
-	// 	for(int j = 0; j <= s2.size(); j++)
-	// 	{
-	// 		cout<<dp[i][j]<<" ";
-	// 	}
-	// 	cout<<endl;
-	// }
-	cout<<dp[s1.size()][s2.size()];
-
-
+int main() {
+    cin >> s1 >> s2;
+    s1 = ' ' + s1;
+    s2 = ' ' + s2;
+    for (int i = 1; i < s1.size(); ++i)
+        for (int j = 1; j < s2.size(); ++j)
+            if (s1[i] == s2[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    cout << dp[s1.size() - 1][s2.size() - 1];
 }
