e017316d205a82e8a80e7449b18bce07372d7cbd
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-07-22 15:29:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,25 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int N, K;
-int W[101], V[101];
-int dp[101][1000001];
+int n, k;
+int dp[100001];
+int w[100], v[100];
 
-int main(void){
-	// 입력
-	cin >> N >> K;
-	for(int i = 1; i <= N; i++)
-		cin >> W[i] >> V[i];
-
-	// 다이나믹 프로그래밍
-	for(int i = 1; i <= N; i++){
-		for(int j = 1; j <= K; j++){
-			if(j >= W[i])
-				dp[i][j] = max(dp[i-1][j - W[i]] + V[i], dp[i-1][j]);
-			else
-				dp[i][j] = dp[i-1][j];
-		}
-	}
-	
-	cout << dp[N][K];
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i)
+        cin >> w[i] >> v[i];
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = k; j >= 0; --j) {
+            if (j - w[i] < 0) break;
+            dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+        }
+    }
+    cout << dp[k];
 }
