52acaed45876a8285fbf07713b95dd717bcec05d
Daolove0323 <90020593+Daolove0323@users.noreply.github.com>
2024-07-24 19:49:24 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 108 ms, Memory: 15460 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,59 +1,63 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <cstring>
+#define USE 1
+#define NOT_USE 0
+#define INF 0x3f3f3f3f
 using namespace std;
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0};
+struct info {
+    int y, x, use;
+};
 
-int N, M, result;
-int map[1000][1000];
-bool visited[1000][1000][2];
+int dy[] = {-1, 1, 0, 0}, dx[] = {0, 0, -1, 1};
+string input;
+int n, m;
+int arr[1000][1000];
+int cnt[2][1000][1000];
+queue<info> q;
 
-void bfs(){
-    queue<vector<int>> q;
-    q.push({0, 0, 1, false});
-    visited[0][0][0] = true;
+int main () {
+    memset(cnt, INF, sizeof(int) * 2 * 1000 * 1000);
+    cin >> n >> m;
+    for (int i = 0 ; i < n; ++i) {
+        cin >> input;
+        for (int j = 0; j < m; ++j)
+            arr[i][j] = input[j] - '0';
+    }
     
-    while(!q.empty()){
-        vector<int> v = q.front();
+    q.push({0, 0, NOT_USE});
+    cnt[NOT_USE][0][0] = cnt[USE][0][0] =  1;
+    
+    while (!q.empty()) {
+        int y = q.front().y;
+        int x = q.front().x;
+        bool use = q.front().use;
+        if (y == n && x == m) break;
         q.pop();
-        if(v[0] == N - 1 && v[1] == M - 1){
-            result = v[2];
-            break;
-        }
         
-        for(int i = 0; i < 4; i++){
-            int ny = v[0] + dy[i];
-            int nx = v[1] + dx[i];
-            if(N <= ny || ny < 0 || M <= nx || nx < 0 ||
-            (v[3] == 1 && map[ny][nx] == 1) ||
-            (v[3] == 1 && visited[ny][nx][1] == 1) ||
-            (v[3] == 0 && visited[ny][nx][0] == 1))
-                continue;
-
-            if(v[3] == 0 && map[ny][nx] == 1){
-                q.push({ny, nx, v[2] + 1, 1});
-                visited[ny][nx][1] = true;
+        for (int i = 0; i < 4; ++i) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (!(0 <= ny && ny < n) || !(0 <= nx && nx < m)) continue; // 범위 밖인 경우
+            if (arr[ny][nx] == 1 && use == USE) continue;   // 벽에 막히는 경우
+            if (cnt[NOT_USE][ny][nx] <= cnt[use][y][x] + 1) continue; // 부수지않았을 때 더 가까운 경우
+            if (use == USE && cnt[USE][ny][nx] <= cnt[USE][y][x] + 1) continue; // 부쉈을  때 더 가까운 경우
+            if (arr[ny][nx] == 1) { // 벽인 경우
+                q.push({ny, nx, USE});  // 부수기
+                cnt[USE][ny][nx] = cnt[NOT_USE][y][x] + 1;  // 부순 경우 기록 갱신
+            } else {    // 빈공간인 경우
+                q.push({ny, nx, use});  // 통과
+                if (use == USE) cnt[USE][ny][nx] = cnt[use][y][x] + 1;  // 부쉈는 경우 기록 갱신
+                else {  // 부수지 않은 경우
+                    cnt[NOT_USE][ny][nx] = cnt[NOT_USE][y][x] + 1;   // 부수지 않은 경우 기록 갱신
+                    cnt[USE][ny][nx] = min(cnt[USE][ny][nx], cnt[use][y][x] + 1);   // 부순 경우 기록 갱신
+                    
                 }
-            else{
-                q.push({ny, nx, v[2] + 1, v[3]});
-                visited[ny][nx][v[3] ? 1 : 0] = true;
-                }
+            }
         }
     }
-}
-
-int main(void){
-    cin >> N >> M;
-    for(int i = 0; i < N; i++)
-        for(int j = 0; j < M; j++)
-            scanf("%1d", &map[i][j]);
-
-    bfs();
     
-    if(visited[N - 1][M - 1][0] || visited[N - 1][M - 1][1])
-        cout << result;
-    else
-        cout << -1;
+    if (cnt[NOT_USE][n - 1][m - 1] == INF && cnt[USE][n - 1][m - 1] == INF) cout << -1;
+    else cout << min(cnt[NOT_USE][n - 1][m - 1], cnt[USE][n - 1][m - 1]);
 }
